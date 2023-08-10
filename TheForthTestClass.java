07152be73d40e54fcec73220b108ae121686f027
mreshta <mreshta10@gmail.com>
2023-08-10 12:20:59 -0700

    adding more codes

---
+++ TheForthTestClass.java
@@ -0,0 +1,11 @@
+package firstProject;
+
+public class TheForthTestClass {
+
+	
+public static void main(String[] args) {
+	System.out.println("This it the forth class for test");
+	String str= "dfdf";
+	
+}
+}
